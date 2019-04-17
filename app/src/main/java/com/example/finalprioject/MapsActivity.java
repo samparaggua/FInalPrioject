7df0d2943f3fb5cb9fdd1c4f7c3c3854c3e4ddeb
samparaggua <sp17@illinois.edu>
2019-04-17 17:58:38 -0500

    final project - got api key

--- app/src/main/java/com/example/finalprioject/MapsActivity.java
+++ app/src/main/java/com/example/finalprioject/MapsActivity.java
@@ -43,7 +43,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-    public void samAndJune(GoogleMap googleMap) {
-        int hi = 0;
-    }
 }
